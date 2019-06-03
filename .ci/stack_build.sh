2e3642bbf78d9eebd4b6c41189583fdccf2b07b2
Leon Schoorl <leon@qbaylogic.com>
2019-06-03 17:07:13 +0200

    Save stack install script to a file
    When run through the CircleCI normal test framework the stack install 
    script seems to be missing the last word of the last line (path).
    
    This results in the error:
    sh: 804: check_home_local_bin_on_: not found
    Exited with code 127
    
    But when runnning interactively or with SSH enabled the problem seems to 
    disappear.

--- .ci/stack_build.sh
+++ .ci/stack_build.sh
@@ -3,5 +3,6 @@ set -xeo pipefail
 
 apt update
 apt install wget -y
-wget -qO- https://get.haskellstack.org/ | sh
+wget -qO- https://get.haskellstack.org/ > install-stack.sh
+sh install-stack.sh || (cat install-stack.sh; false)
 stack build
